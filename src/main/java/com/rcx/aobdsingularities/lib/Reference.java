483aeffbc6907b7cae7f2c073324b2c40cfedc84
RCXcrafter <rcxcrafter@gmail.com>
2016-10-23 20:42:08 +0200

    Fixed config gui mistake and increased version

--- src/main/java/com/rcx/aobdsingularities/lib/Reference.java
+++ src/main/java/com/rcx/aobdsingularities/lib/Reference.java
@@ -5,7 +5,7 @@ public class Reference {
 	public static final String MOD_ID = "aobdsingularities";
 	public static final String MOD_NAME = "Another One Bites The Dust: Singularities";
 	public static final String DEPENDENCIES = "required-before:aobd;required-after:Avaritia";
-	public static final String VERSION_NUMBER = "1.0.0";
+	public static final String VERSION_NUMBER = "1.0.1";
 
-	public static final String GUI_FACTORY_CLASS = "com.rcx.aobdsingularities.configuration.ConfigGuiFactory";
+	public static final String GUI_FACTORY_CLASS = "com.rcx.aobdsingularities.config.ConfigGuiFactory";
 }
