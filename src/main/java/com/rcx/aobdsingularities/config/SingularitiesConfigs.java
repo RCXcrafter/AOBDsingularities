5ca3509758a8c0f605d2236f887c093f883c6aaf
RCXcrafter <rcxcrafter@gmail.com>
2016-10-25 12:41:37 +0200

    Add default values for many materials

--- src/main/java/com/rcx/aobdsingularities/config/SingularitiesConfigs.java
+++ src/main/java/com/rcx/aobdsingularities/config/SingularitiesConfigs.java
@@ -7,11 +7,14 @@ public class SingularitiesConfigs {
 
 	public SingularitiesConfigs(String name) {
 
-		if ("cobalt".equals(name) || "ardite".equals(name)) {
-			//I need to come up with some defaults for common ores
-			amountNeeded = 400;
-		} else if ("endium".equals(name)) {
-			amountNeeded = 400;
+		if ("atlarus".equals(name) || "adamantine".equals(name) || "sanguinite".equals(name) || "meutoite".equals(name) || "iridium".equals(name) || "plutonium".equals(name)) {
+			amountNeeded = 50;
+		} else if ("platinum".equals(name) || "orichalcum".equals(name) || "kalendrite".equals(name) || "vulcanite".equals(name) || "eximite".equals(name)) {
+			amountNeeded = 100;
+		} else if ("cobalt".equals(name) || "ardite".equals(name) || "mithril".equals(name) || "rubracium".equals(name) || "ceruclase".equals(name) || "alduorite".equals(name) || "osmium".equals(name) || "tungsten".equals(name) || "titanium".equals(name) || "uranium".equals(name)) {
+			amountNeeded = 200;
+		} else if ("endium".equals(name) || "manganese".equals(name) || "oureclase".equals(name) || "astralsilver".equals(name) || "carmot".equals(name) || "midasium".equals(name) || "vyroxeres".equals(name) || "darkiron".equals(name)) {
+			amountNeeded = 300;
 		} else {
 			amountNeeded = 400;
 		}
